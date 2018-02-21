9543c1baac1bba53b450cba555d607ef5841f1ae
Mujib517 <mujib517@gmail.com>
2018-02-21 08:53:10 +0530

    Routing in NodeJS

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var http = require('http'); //common js. AMD  define([],function(){})
+var fs = require('fs');
 
 function handleReq(req, res) {
-    res.write("Hello NodeJS");
-    res.end();
+
+    switch (req.url) {
+        case '/':
+            var contents = fs.readFileSync("index.html");
+            res.write(contents);
+            res.end();
+            break;
+        case '/products':
+            res.write("Products");
+            res.end();
+            break;
+        case '/books':
+            var books = [{ id: 1, name: "Speaking JS", price: 100 },
+            { id: 2, name: "Algorithms and DS", price: 50 },
+            { id: 3, name: "Eloquent JS", price: 80 }];
+
+            res.write(JSON.stringify(books));
+            res.end();
+            break;
+        default:
+            res.write("Hello NodeJS");
+            res.end();
+            break;
+    }
 }
 
 var server = http.createServer(handleReq);
@@ -10,6 +33,7 @@ var server = http.createServer(handleReq);
 function callback() {
     console.log("Server is running on 3000");
 }
+
 
 server.listen(3000, callback);
 
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 To run this app
 1. npm install
 2. node index.js
+
